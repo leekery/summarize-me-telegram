e5c8dda43bffb2c252c7baa3d4ab73338f0efe2d
leekery <pashokmoreplay@gmail.com>
2025-07-28 19:46:40 +0300

    Major: Full logic for config.py with dataclass/pydantic realization

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from dotenv import load_dotenv
 
 load_dotenv()
-
+# Comment this block to use pydantic
 @dataclass(frozen=True)
 class Settings:
     bot_token: str
@@ -19,9 +19,29 @@ class Settings:
             deepseek_api=os.environ["DEEPSEEK_API_KEY"],
             db_path=os.environ["DB_PATH"]
         )
-    
 settings = Settings.from_env()
 
 # How to use | dataclass
+# from config import settings
+# token = settings.bot_token
+# print(token)
+
+# Commnet this block to use dataclass
+# from pydantic_settings import BaseSettings
+# class Settings(BaseSettings):
+#     bot_token: str
+#     admin_id: int
+#     deepseek_api_key: str
+#     db_path: str
+#     default_limit: int
+
+#     model_config = {
+#         "env_file": ".env",
+#         "extra": "ignore",
+#     }
+
+# settings = Settings() # type: ignore
+
+# How to use | pydantic
 # from config import settings
 # token = settings.bot_token
