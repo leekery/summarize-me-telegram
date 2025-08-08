64be0ba6d9da473365758194cad758bd010f3e6c
leekery <pashokmoreplay@gmail.com>
2025-08-08 03:03:54 +0300

    Fix: collector.py now do not register commands

--- bot/handlers/collector.py
+++ bot/handlers/collector.py
@@ -5,14 +5,18 @@ from bot.db.repo import MessageRepo
 router = Router()
 repo = MessageRepo()
 
-# слушаются только группы/супергруппы и только текст
-@router.message(F.chat.type.in_({"group", "supergroup"}) & F.text)
-async def collect_messages(message: Message) -> None:
+# слушаются только группы/супергруппы и только текст, исключая команды
+@router.message(
+    F.chat.type.in_({"group", "supergroup"}) 
+    & F.text 
+    & ~F.text.startswith("/")
+)
+async def collect_messages(message: Message):
     user = message.from_user
     await repo.save_message(
         chat_id=message.chat.id,
         user_id=user.id if user else None,
-        user_name=(user.full_name if user else "Anonymous"),
+        user_name=user.full_name if user else "Anonymous",
         text=message.text,
         timestamp=message.date.isoformat(),
     )
